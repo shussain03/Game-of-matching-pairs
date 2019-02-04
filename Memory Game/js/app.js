20840087d04240821d0d3cc381c94abfa4e4104d
shussain03 <46450877+shussain03@users.noreply.github.com>
2019-02-04 17:01:00 +0000

    updated tags & working on flipping card over

--- Memory Game/js/app.js
+++ Memory Game/js/app.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
+  console.log("working");
+
+
+var images = ['img1(<img src="images/pairs/1aln.png">)', img2
+var img3 =
+var img4 = 
+var img5 =
+var img4 = 
+
+
+var imgArray = ["img1", "img2", "img3", "img4", "img5", "img6"]
 
 
 
 
 
+<p id="img1" onclick="showPicture()">Click me to change my text color.</p>
+
+<script>
+function showPicture() {
+  document.getElementById("img1");
+  
+}
+</script>
 
 
-
-
-
-
-
-
-
+//https://3dtransforms.desandro.com/card-flip
 
 
 
