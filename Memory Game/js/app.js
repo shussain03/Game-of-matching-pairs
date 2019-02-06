3787e1296c6cf4197f7b90d556225f5c5d33f998
shussain03 <46450877+shussain03@users.noreply.github.com>
2019-02-06 16:41:36 +0000

    working game of pairs

--- Memory Game/js/app.js
+++ Memory Game/js/app.js
@@ -1,37 +1,66 @@
-$(document).ready(function () {
+document.addEventListener("DOMContentLoaded", function () {
   console.log("working");
 
-
-var images = ['img1(<img src="images/pairs/1aln.png">)', img2
-var img3 =
-var img4 = 
-var img5 =
-var img4 = 
+  const cards = document.querySelectorAll('.memory-card'); /*
+  -memory-card elements in the CSS file will also be called by const cards = document.querySelectorAll in the app.js file.
+  -const - has been used as a unique id(edentifier in this game
+    */
 
 
-var imgArray = ["img1", "img2", "img3", "img4", "img5", "img6"]
+  var hasFlippedCard = false; // hasflipped card & flippedcard manages the flip state incase no cards are flipped over it is et to 'true' and set to the card being clicked.
+  var lockBoard = false;
+  var firstCard, secondCard;
 
+  function flipCard() {
+    if (lockBoard) return; /*this freezes the first card*/
+    if (this === firstCard) return; 
 
+    this.classList.add('flip');
 
+    if (!hasFlippedCard) {
+      //on first click
+      hasFlippedCard = true;
+      firstCard = this;
+      return;
+    }
+      // second click
+    secondCard = this;
 
+    checkForMatch();
+  }
 
-<p id="img1" onclick="showPicture()">Click me to change my text color.</p>
+  /*to add extrainformation in an html, use dataset.framework*/
+  function checkForMatch() {
+    var isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards(); zz
+  }
 
-<script>
-function showPicture() {
-  document.getElementById("img1");
-  
-}
-</script>
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
+   resetBoard();
+  }
 
-//https://3dtransforms.desandro.com/card-flip
+  //this is a prblem to be solved
+  function unflipCards() {
+    lockBoard = true;
 
+    setTimeout(function() {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
 
+    lockBoard = false;
+     resetBoard();
+    }, 1500);
+  }
 
+ function resetBoard() {
+   [hasFlippedCard, lockBoard] = [false, false];
+   [firstCard, secondCard] = [null, null];
+ }
 
-
-
+  cards.forEach(card => card.addEventListener('click',flipCard));  //
 
 
 
